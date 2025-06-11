5a11d14c113a99f26c82cd4c0521b912714058df
tilobaaa <adedolaposamuel21@gmail.com>
2025-06-11 10:05:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className=' flex flex-start'>
+      <p>let us start what we came into the room to do</p>
+    </div>
+  )
+}
+
+export default App

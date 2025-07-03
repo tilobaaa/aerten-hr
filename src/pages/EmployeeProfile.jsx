3d0b42a9daa6b220ced440df85a08798f5784b13
tilobaaa <adedolaposamuel21@gmail.com>
2025-07-03 20:29:50 +0100

    adding the employees profile page

--- src/pages/EmployeeProfile.jsx
+++ src/pages/EmployeeProfile.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { employees } from "../assets/requestArray";
 import { useParams } from "react-router-dom";
 import arrowBackSvg from "../assets/arrow-back.svg";
 import { Link } from "react-router-dom";
-import editSvg from '../assets/edit-2.svg'
-import helpSvg from '../assets/help-circle.svg';
-import moreVerticalSvg from '../assets/more-horizontal.svg'
+import editSvg from "../assets/edit-2.svg";
+import helpSvg from "../assets/help-circle.svg";
+import moreVerticalSvg from "../assets/more-horizontal.svg";
 
 const EmployeeProfile = () => {
   const { id } = useParams();
   const employee = employees.find((employee) => employee.id === id);
-  console.log(employee);
+  const [clickedOption, setClickedOption] = useState('general')
 
   return (
     <div className="my-[1.75rem] mx-[3.25rem] flex flex-col gap-6">
@@ -23,8 +23,12 @@ const EmployeeProfile = () => {
       </Link>
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-[0.94rem]">
-          <h4 className="text-2xl text-[#101828] font-semibold">{employee.name}</h4>
-          <p className="px-[0.38rem] text-xs font-medium text-[#7a8699] rounded-2xl bg-[#f5f6f7]">#Employee ID: <span className="text-[#091E42] ">{employee.id}</span></p>
+          <h4 className="text-2xl text-[#101828] font-semibold">
+            {employee.name}
+          </h4>
+          <p className="px-[0.38rem] text-xs font-medium text-[#7a8699] rounded-2xl bg-[#f5f6f7]">
+            #Employee ID: <span className="text-[#091E42] ">{employee.id}</span>
+          </p>
         </div>
         <div className="flex items-center gap-[0.94rem]">
           <div className="flex items-center gap-2 px-4 py-[0.62rem] border-1 border-[#E6e7ec] rounded-md  ">
@@ -36,6 +40,16 @@ const EmployeeProfile = () => {
             <img className="" src={moreVerticalSvg} alt="" />
           </div>
         </div>
+      </div>
+
+      {/* buttons */}
+      <div className="flex items-center gap-6 border-b-1 border-[#eaecf0]">
+        <button onClick={()=>{setClickedOption('general')}} className={`cursor-pointer text-sm font-medium pb-1 ${clickedOption === 'general' ? 'text-[#6a1039] border-b-1 border-[#6a1039]':'text-[#667085]' }`}>General Info</button>
+        <button onClick={()=>{setClickedOption('documents')}} className={`cursor-pointer text-sm font-medium pb-1 ${clickedOption === 'documents' ? 'text-[#6a1039] border-b-1 border-[#6a1039]':'text-[#667085]' }`}>Documents</button>
+        <button onClick={()=>{setClickedOption('reminders')}} className={`cursor-pointer text-sm font-medium pb-1 ${clickedOption === 'reminders' ? 'text-[#6a1039] border-b-1 border-[#6a1039]':'text-[#667085]' }`}>Reminders</button>
+        <button onClick={()=>{setClickedOption('performance')}} className={`cursor-pointer text-sm font-medium pb-1 ${clickedOption === 'performance' ? 'text-[#6a1039] border-b-1 border-[#6a1039]':'text-[#667085]' }`}>Performance</button>
+        <button onClick={()=>{setClickedOption('salary')}} className={`cursor-pointer text-sm font-medium pb-1 ${clickedOption === 'salary' ? 'text-[#6a1039] border-b-1 border-[#6a1039]':'text-[#667085]' }`}>Salary and benefits</button>
+        <button onClick={()=>{setClickedOption('feedback')}} className={`cursor-pointer text-sm font-medium pb-1 ${clickedOption === 'feedback' ? 'text-[#6a1039] border-b-1 border-[#6a1039]':'text-[#667085]' }`}>Feedback</button>
       </div>
     </div>
   );

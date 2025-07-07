0873c7319804a9ab909717ba9f5fbb8a835aabbb
tilobaaa <adedolaposamuel21@gmail.com>
2025-07-07 22:03:26 +0100

    permissions first update

---
+++ src/components/Roles/Permissions Folder/AllEmployees.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ToggleButton from '../../utilities/ToggleButton'
+
+const AllEmployees = () => {
+  return (
+    <div className='py-[1.62rem] px-[1.44rem] border-b-1 border-[#eff1f4]'>
+        <div className='flex items-center justify-between'>
+            <p className='text-sm text-[#8993a4] font-semibold'>Manager Access</p>
+            <div className='flex items-center gap-[0.12rem]'>
+                <ToggleButton/>
+                <p className='text-sm text-black font-medium'>Enable all</p>
+            </div>
+        </div>
+
+        <div className='my-6 flex flex-wrap items-center '>
+            <div className='basis-1/2 flex gap-3 items-center px-2  border-b-1 border-[#eff1f4]'>
+                <ToggleButton/>
+                <div className='flex flex-col py-4'>
+                    <p className='text-[#091e42] text-sm font-bold'>Access to new hires</p>
+                    <p className='text-sm text-[#909dad]'>Can create job posting and manage new hires</p>
+                </div>
+            </div>
+            <div className=' basis-1/2 flex gap-3 items-center   border-b-1 border-[#eff1f4]'>
+                <ToggleButton/>
+                <div className='flex flex-col py-4'>
+                    <p className='text-[#091e42] text-sm font-bold'>Can see information about other employees</p>
+                    <p className='text-sm text-[#909dad]'>Enables access to view employee profile informations. Doesn’t permit edit access</p>
+                </div>
+            </div>
+            {/* <div className='flex gap-3 items-center basis-1/2 border-b-1 border-[#eff1f4]'>
+                <ToggleButton/>
+                <div className='flex flex-col py-4'>
+                    <p className='text-[#091e42] text-sm font-bold'>Can add or delete employee</p>
+                    <p className='text-sm text-[#909dad]'>This enables general access to employee information</p>
+                </div>
+            </div>
+            <div className='flex gap-3 items-center basis-1/2 border-b-1 border-[#eff1f4]'>
+                <ToggleButton/>
+                <div className='flex flex-col py-4'>
+                    <p className='text-[#091e42] text-sm font-bold'>Can add/edit roles</p>
+                    <p className='text-sm text-[#909dad]'>This enables general access to creating or modifying roles</p>
+                </div>
+            </div> */}
+
+        </div>
+    </div>
+  )
+}
+
+export default AllEmployees
--- src/components/Roles/Permissions.jsx
+++ src/components/Roles/Permissions.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import helpSvg from "../../assets/help-circle.svg";
+import AllEmployees from "./Permissions Folder/AllEmployees";
 
 const Permissions = () => {
+  const [clickedOption, setClickedOption] = useState("employees");
   return (
-    <div className="py-[2.37rem] px-[1.88rem]">
-      <div className="flex flex-col gap-3">
+    <div className="my-[2.44rem] py-[2.37rem] px-[1.88rem] rounded-[0.625rem] border-1 border-[#eff1f4] ">
+      <div className="flex flex-col gap-3 border-b-1 border-[#eff1f4] pb-[1.56rem]">
         <div className="flex items-start w-fit">
           <p className="text-[#0e2354] text-lg font-semibold">Owner (Admin)</p>
           <div className="relative group">
-            <img className="w-4 h-4 rounded-full cursor-pointer " src={helpSvg} alt="" />
+            <img
+              className="w-4 h-4 rounded-full cursor-pointer "
+              src={helpSvg}
+              alt=""
+            />
             <p className="hidden group-hover:block py-[0.68rem] px-[1.625rem] text-xs font-medium text-[#676e7e] rounded-[1.25rem] bg-[#eff1f4] w-[17rem] absolute">
               Every member gets basic permissions and functionality by default.
               You can customize settings for all members and teams or make local
@@ -21,6 +27,81 @@ const Permissions = () => {
           be changed
         </p>
       </div>
+
+      <div className="px-[1.88rem] py-[1.94rem]">
+        <div className="flex flex-col gap-3 pb-8 border-b-1 border-[#eff1f4]">
+          <p className="text-lg font-semibold text-[#0e2354]">
+            Set permissions
+          </p>
+          <p className="text-sm text-[#676e7e]">
+            Modify access and permission for teams and individuals{" "}
+          </p>
+        </div>
+
+        <div className="pt-[2.13rem] border-b-1 border-[#EAECF0] flex gap-4 ">
+          <button
+            onClick={() => {
+              setClickedOption("teams");
+            }}
+            className={`cursor-pointer text-sm font-medium pb-4 ${
+              clickedOption === "teams"
+                ? "text-[#6A1039] border-b-2 border-[#6A1039]"
+                : "text-[#667085]"
+            }`}
+          >
+            For all teams
+          </button>
+          <button
+            onClick={() => {
+              setClickedOption("roles");
+            }}
+            className={`cursor-pointer text-sm font-medium pb-4 ${
+              clickedOption === "roles"
+                ? "text-[#6A1039] border-b-2 border-[#6A1039]"
+                : "text-[#667085]"
+            }`}
+          >
+            For all roles
+          </button>
+          <button
+            onClick={() => {
+              setClickedOption("employees");
+            }}
+            className={`cursor-pointer text-sm font-medium pb-4 ${
+              clickedOption === "employees"
+                ? "text-[#6A1039] border-b-2 border-[#6A1039]"
+                : "text-[#667085]"
+            }`}
+          >
+            For all employees
+          </button>
+          <button
+            onClick={() => {
+              setClickedOption("admins");
+            }}
+            className={`cursor-pointer text-sm font-medium pb-4 ${
+              clickedOption === "admins"
+                ? "text-[#6A1039] border-b-2 border-[#6A1039]"
+                : "text-[#667085]"
+            }`}
+          >
+            For Admins
+          </button>
+          <button
+            onClick={() => {
+              setClickedOption("custom");
+            }}
+            className={`cursor-pointer text-sm font-medium pb-4 ${
+              clickedOption === "custom"
+                ? "text-[#6A1039] border-b-2 border-[#6A1039]"
+                : "text-[#667085]"
+            }`}
+          >
+            Custom
+          </button>
+        </div>
+      </div>
+        <div>{clickedOption === "employees" && <AllEmployees />}</div>
     </div>
   );
 };
